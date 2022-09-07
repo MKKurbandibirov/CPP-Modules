3289602b4bb74f298e47431f94a0decb5d54da96
Napoleon Farfetch <nfarfetc@va-c5.kzn.21-school.ru>
2022-09-07 10:35:28 +0300

    Add ex02

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -3,5 +3,59 @@
 
 # include <iostream>
 
+template<typename T>
+class Array {
+private:
+	T* arr;
+	unsigned int N;
+public:
+	Array( void ) {
+		this->arr = NULL;
+		this->N = 0;
+	}
+	Array( unsigned int N ) {
+		if (N < 0) {
+			throw std::exception();
+		} else if (N == 0) {
+			this->arr = NULL;
+			this->N = 0;
+		} else {
+			this->arr = new T[N];
+			this->N = N;
+		}
+	}
+	~Array( void ) {
+		delete[] this->arr;
+	}
+	Array( const Array& other ) {
+		delete[] this->arr;
+		this->arr = new T(other.size());
+		for (int i = 0; i < (int)other.size(); i++) {
+			this->arr[i] = other[i];
+		}
+	}
+	Array& operator=( const Array& other) {
+		if (this == &other) {
+			return *this;
+		}
+		delete[] this->arr;
+		this->arr = new T(other.size());
+		for (int i = 0; i < (int)other.size(); i++) {
+			this->arr[i] = other[i];
+		}
+		return *this;
+	}
+
+	T& operator[](unsigned int idx) const {
+		if (this->size() - 1 < idx || idx < 0) {
+			throw std::exception();
+		}
+		return this->arr[idx];
+	}
+
+	unsigned int size() const {
+		return this->N;
+	}
+};
 
 #endif
--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -1,7 +1,19 @@
 #include "Array.hpp"
 
 int main() {
-    int *a = new int();
+    Array<int> arr(5);
 
-    std::cout << *a;
+    std::cout << arr.size() << std::endl;
+    arr[2] = 90;
+    std::cout << arr[2] << std::endl;
+
+
+    Array<int> arr1(12);
+    arr1[1] = 88;
+
+    arr = arr1;
+
+    std::cout << arr[1] << std::endl;
+
+    
 }
